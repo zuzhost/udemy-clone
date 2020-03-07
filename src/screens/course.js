a4b125efc5919984bf7480518c5a022948e47296
Kamran Wajdani <kamranwajdani@gmail.com>
2020-03-07 19:16:49 +0500

    Updated to Tutorial # 5

--- src/screens/course.js
+++ src/screens/course.js
@@ -126,6 +126,74 @@ function CoursePage(){
                     </div>
                 </div>
 
+                <div className="extras-block rel flex">
+
+                    <div className="chat rel">
+                        <div className="section section-b rel">                    
+                            <h2 className="title s24 fontb">Quick <span  className="fontn">Chat</span></h2>
+                            <div className="messages aic flex">
+                                <div className="bubble rel">
+                                    <h2 className="txt ibl fontn s15 c333">I am a newbie</h2>                                    
+                                </div>
+                                <div className="bubble rel">
+                                    <h2 className="txt ibl fontn s15 c333">Love this course...</h2>                                    
+                                </div>
+                                <div className="bubble bubble-mine rel">
+                                    <h2 className="txt ibl fontn s15 c333">Hey</h2>                                    
+                                </div>
+                                <div className="bubble bubble-mine rel">
+                                    <h2 className="txt ibl fontn s15 c333">Dope Intro, thanks!@</h2>                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="now-watching rel">
+                        <div className="tooltip abs s13 fontb cfff">86 Live Now</div>
+                        <div className="section section-b rel">                    
+                            <h2 className="title s24 fontb">Now <span  className="fontn">Watching</span></h2>
+                            <div className="you-list rel">
+                                <div className="you rel aic flex">
+                                    <div className="pic">
+                                        <img src="http://placeimg.com/100/100/people?guest-1" className="bl" />
+                                    </div>
+                                    <div className="meta rel">
+                                        <h2 className="s15 name fontb c333">Grey Montgommery</h2>
+                                        <h2 className="s13 uname fontn c777">@gregdomm</h2>
+                                    </div>
+                                </div>
+                                <div className="you rel aic flex">
+                                    <div className="pic">
+                                        <img src="http://placeimg.com/100/100/people?guest-2" className="bl" />
+                                    </div>
+                                    <div className="meta rel">
+                                        <h2 className="s15 name fontb c333">Leila Jefferson</h2>
+                                        <h2 className="s13 uname fontn c777">@leilaearns</h2>
+                                    </div>
+                                </div>
+                                <div className="you rel aic flex">
+                                    <div className="pic">
+                                        <img src="http://placeimg.com/100/100/people?guest-3" className="bl" />
+                                    </div>
+                                    <div className="meta rel">
+                                        <h2 className="s15 name fontb c333">Leila Jefferson</h2>
+                                        <h2 className="s13 uname fontn c777">@leilaearns</h2>
+                                    </div>
+                                </div>
+                                <div className="you rel aic flex">
+                                    <div className="pic">
+                                        <img src="http://placeimg.com/100/100/people?guest-4" className="bl" />
+                                    </div>
+                                    <div className="meta rel">
+                                        <h2 className="s15 name fontb c333">Leila Jefferson</h2>
+                                        <h2 className="s13 uname fontn c777">@leilaearns</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
 
         </div>
